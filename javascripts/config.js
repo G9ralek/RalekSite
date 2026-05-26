a7e3f2686faae1eb575b9b83a670c4383fb2da93
R4lek <168626495+R4lek@users.noreply.github.com>
2026-05-26 19:42:13 +0200

    Rename twitchClip to twitchClipUrl and update URL

--- javascripts/config.js
+++ javascripts/config.js
@@ -1,5 +1,5 @@
-const twitchClip =
-"https://clips.twitch.tv/embed?clip=FunnyClip123&parent=r4lek.github.io";
+const twitchClipUrl =
+"https://www.twitch.tv/r4lek/clip/ClumsyGloriousTraySwiftRage-A8y_WssEMygBRISM";
 
 const tiktokVideo =
 "https://www.tiktok.com/@ttv.r4lek/video/123456789";
